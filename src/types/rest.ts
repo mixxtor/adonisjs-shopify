017edbe6ea4be8cc304724825cac5c8b50eed083
mixxtor <113852548+mixxtor@users.noreply.github.com>
2025-08-17 20:14:38 +0700

    fix: update Shopify API types and dependencies for consistency

--- src/types/rest.ts
+++ src/types/rest.ts
@@ -1,21 +1,20 @@
 import ShopifyProvider from '../../providers/shopify_provider.js'
-import { Session, ApiVersion, ShopifyClients } from '@shopify/shopify-api'
+import { Session, ApiVersion, ShopifyClients, ShopifyRestResources } from '@shopify/shopify-api'
 import { SHOPIFY } from '../constants/shopify.js'
-import type { RestResources } from '@shopify/shopify-api/rest/admin/2025-07'
 export * from './resource.js'
 
 // type RestResources = Record<string, any>
 
-type ShopifyRestInstances<Resources extends RestResources = RestResources> = {
+type ShopifyRestInstances<Resources extends ShopifyRestResources> = {
   [K in keyof Resources]: InstanceType<Resources[K]>
 }
 
 type TShopifyPlan = (typeof SHOPIFY.PLAN.UNIT)[keyof typeof SHOPIFY.PLAN.UNIT]
-export interface Shopify extends ShopifyRestInstances<RestResources> {
+export interface Shopify extends ShopifyRestInstances<ShopifyRestResources> {
   ShopifyProvider: typeof ShopifyProvider
   Session: Session
   ApiVersion: ApiVersion
-  RestResources: RestResources
+  RestResources: ShopifyRestResources
   RestClient: ShopifyClients['Rest']
   GraphqlClient: ShopifyClients['Graphql']
   StorefrontClient: ShopifyClients['Storefront']

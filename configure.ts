d2cccbdfd6bef4a466565dcac9e2e3c8355ed757
mixxtor <113852548+mixxtor@users.noreply.github.com>
2025-08-26 14:38:04 +0700

    fix: handle undefined value for SHOPIFY_API_EXTRA_APPS in configuration

--- configure.ts
+++ configure.ts
@@ -36,7 +36,7 @@ export async function configure(command: ConfigureCommand) {
         SHOPIFY_API_VERSION: 'Env.schema.string()',
         SHOPIFY_API_SCOPES: 'Env.schema.string()',
         SHOPIFY_API_EXTRA_APPS: `(name: string, value: string | undefined) => {
-          const apps = value?.split(',')
+          const apps = value ? value.split(',') : []
           if (value && apps?.every((v) => v.split(':').length !== 2)) {
             throw new Error(\`Value for \${name} must be a valid format: KEY_1:SECRET_1,KEY_2:SECRET_2,KEY_3:SECRET_3...\`)
           }
